78db44b04983f3188dab4af81d76b30c7cb896c0
BilalHussain009 <32794987+BilalHussain009@users.noreply.github.com>
2017-10-14 17:41:10 +0500

    Add files via upload

---
+++ 99999999.py
@@ -0,0 +1,9 @@
+a=9
+while a>=1:
+    b=a
+    while b>=1:
+        print(a,end="")
+        b=b-1
+    print()
+    a=a-1
+        
---
+++ factorial.py
@@ -0,0 +1,7 @@
+a=int(input("enter a number"))
+fact=1
+while a>=1:
+    b=a
+    fact=fact*b
+    a=a-1
+print("the factorial is",fact)
